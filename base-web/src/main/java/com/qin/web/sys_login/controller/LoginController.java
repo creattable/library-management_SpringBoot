e39faab9667591b85efc1d6d1a1697b9ad3ef990
QINJIALE <940403048@qq.com>
2022-03-24 19:27:43 +0800

    可以使用不同的角色登录

--- base-web/src/main/java/com/qin/web/sys_login/controller/LoginController.java
+++ base-web/src/main/java/com/qin/web/sys_login/controller/LoginController.java
@@ -53,7 +53,7 @@ public class LoginController {
         
         //加密后的密码
         String s = DigestUtils.md5DigestAsHex(loginParm.getPassword().getBytes());
-        System.out.println(s+"--------------------------------");
+        System.out.println(s + "--------------------------------");
         
         //判断是读者还是管理
         //传来的参数中0是读者，1是管理
@@ -102,52 +102,52 @@ public class LoginController {
     
     //获取用户权限字段
     @GetMapping("/getInfo")
-    public ResultVo getInfo(Long userid, HttpServletRequest request){
-        
+    public ResultVo getInfo(Long userId, HttpServletRequest request) {
         //从请求的头部获取token
-        String token=request.getHeader("token");
-        //获取从token解析用户的类型
+        String token = request.getHeader("token");
+        //从token里面解析用户的类型
         Claims claims = jwtUtils.getClaimsFromToken(token);
         Object userType = claims.get("userType");
-        //定义用户信息类(还是接收用的)
-        UserInfo userInfo=new UserInfo();
-        
+        //定义用户信息类
+        UserInfo userInfo = new UserInfo();
+
         //0表示读者，1是管理
-        if (userType.equals("0")){
+        if (userType.equals("0")) { //读者
             //根据id查询读者信息
-            SysReader reader = sysReaderService.getById(userid);
+            SysReader reader = sysReaderService.getById(userId);
             userInfo.setIntroduction(reader.getLearnNum());
             userInfo.setName(reader.getLearnNum());
             userInfo.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
-            //权限字段的查询
-            List<SysMenu> menuList = sysMenuService.getReaderMenuByUserId(userid);
+            //权限字段查询与设置
+            List<SysMenu> menuList = sysMenuService.getReaderMenuByUserId(userId);
             //只需要权限字段，因此需要过滤不需要的
             List<String> collect = menuList.stream().filter(item -> item != null && item.getCode() != null).map(item -> item.getCode()).collect(Collectors.toList());
-            //转成数组然后返回
+            if (collect.size() == 0) {
+                return ResultUtils.error("暂无登录权限，请联系管理员!");
+            }
+            //转成数组
             String[] strings = collect.toArray(new String[collect.size()]);
             userInfo.setRoles(strings);
-            return ResultUtils.success("查询成功",userInfo);
-    
-    
-        }else if (userType.equals("1")){
-            SysUser user = sysUserService.getById(userid);
+            return ResultUtils.success("查询成功", userInfo);
+        } else if (userType.equals("1")) { //管理员
+            SysUser user = sysUserService.getById(userId);
             userInfo.setIntroduction(user.getNickName());
             userInfo.setName(user.getNickName());
             userInfo.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
-            //权限字段的查询
-            List<SysMenu> menuList = sysMenuService.getMenuByUserId(userid);
+            //权限字段查询与设置
+            List<SysMenu> menuList = sysMenuService.getMenuByUserId(userId);
             //只需要权限字段，因此需要过滤不需要的
             List<String> collect = menuList.stream().filter(item -> item != null && item.getCode() != null).map(item -> item.getCode()).collect(Collectors.toList());
-            //转成数组然后返回
+            if (collect.size() == 0) {
+                return ResultUtils.error("暂无登录权限，请联系管理员!");
+            }
+            //转成数组
             String[] strings = collect.toArray(new String[collect.size()]);
             userInfo.setRoles(strings);
-            return ResultUtils.success("查询成功",userInfo);
-        
-        }else {
-            return ResultUtils.error("用户类型不存在",userInfo);
+            return ResultUtils.success("查询成功", userInfo);
+        } else {
+            return ResultUtils.error("用户类型不存在", userInfo);
         }
-    
-    
     }
     
 }
